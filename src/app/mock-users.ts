ae3a470fe48dd7121a0736bd5a05152060a70528
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2023-01-31 00:21:14 +0100

    add mock users

---
+++ src/app/mock-users.ts
@@ -0,0 +1,8 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+    { id: 1, name: 'Jan', surname: 'Niski', phone: '123456789', email: ''},
+    { id: 2, name: 'Adam', surname: 'Wysoki', phone: '123456789', email: ''},
+    { id: 3, name: 'Ada', surname: 'Chuda', phone: '123456789', email: ''},
+    { id: 4, name: 'Krzyś', surname: 'Wąski', phone: '123456789', email: ''}
+]
