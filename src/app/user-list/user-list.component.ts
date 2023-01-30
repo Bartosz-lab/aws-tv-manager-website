4cb7d80709aa1a64f6cd7da2ad0b9e92efb20d60
Bartosz Cieślik <bartoszcieslik2@gmail.com>
2023-01-31 00:11:30 +0100

    add user list

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { UserService } from '../user.service';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent {
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers()
+      .subscribe(users => this.users = users);
+  }
+
+  users: User[] = [];
+}
